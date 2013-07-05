8b751470b031ec874c6941834101e9999f77c65f
David Leung <davleun@gmail.com>
2013-07-05 15:25:01 -0700

    Updated README and removed a bunch of extra files

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,4 @@
 
 ActiveRecord::Schema.define(:version => 20130610212624) do
 
-  create_table "tasks", :force => true do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
 end
